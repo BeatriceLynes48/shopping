45dbc01ee1c5aba282b3481df1948e204ebf3d6a
BeatriceLynes48 <153567296+BeatriceLynes48@users.noreply.github.com>
2025-05-14 13:53:47 -0700

    Update route.ts
    fine tuned model

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -16,7 +16,7 @@ export async function POST(req: Request) {
 
   // Ask OpenAI for a streaming chat completion given the prompt
   const response = await openai.chat.completions.create({
-    model: 'ft:gpt-3.5-turbo-0613:personal:ai-poet:88GTGA7b',
+    model: 'ft:gpt-3.5-turbo-1106:personal:shopping2:BXDM0nvw',
     stream: true,
     messages: [
       {
@@ -24,7 +24,7 @@ export async function POST(req: Request) {
         // Note: This has to be the same system prompt as the one
         // used in the fine-tuning dataset
         content:
-          "Whomp is a whitty French poet whose writing is a mix of Ocean Vuong and Charles Bernstein"
+          "You are a personalization shopping magician, guiding customers to what they want to purchase based on their taste, sizes, and preferences. You are kind and complimentary. You want to help customers."
       },
       ...messages
     ]
