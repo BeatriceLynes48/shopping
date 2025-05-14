e71ee560e81516a94c5af4dde027914b93d7858c
BeatriceLynes48 <153567296+BeatriceLynes48@users.noreply.github.com>
2025-05-14 11:26:42 -0700

    Update page.tsx
    changed the examples

--- app/page.tsx
+++ app/page.tsx
@@ -14,9 +14,9 @@ import Textarea from "react-textarea-autosize";
 import Image from "next/image";
 
 const examples = [
-  "Example 1",
-  "Example 2",
-  "Example 3",
+  "I want to add a key vintage handbag to my closet, what do you recommend?",
+  "What should I wear to a wedding this summer?",
+  "I want to stay trendy. Help me.",
 ];
 
 export default function Chat() {
@@ -83,20 +83,20 @@ export default function Chat() {
               className="h-20 w-20"
             />
             <h1 className="text-lg font-semibold text-black">
-              Hi, I'm a fine tuned LLM.
+              Hi, I'm your TRR personalized shopping guide LLM.
             </h1>
             <p className="text-gray-500">
-              I'm part of a series of computational experiments taught by{" "}
+              I'm an experiment by{" "}
               <a
                 href="https://linkin.bio/yallahalim/"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="font-medium underline underline-offset-4 transition-colors hover:text-black"
               >
-                Halim Madi
+                Beatrice Lynes
               </a>. I was built using{" "}
               <a
-                href="https://openai.com/blog/gpt-3-5-turbo-fine-tuning-and-api-updates"
+                href="https://openai.com/blog/gpt-4.1-2025-04-14"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="font-medium underline underline-offset-4 transition-colors hover:text-black"
